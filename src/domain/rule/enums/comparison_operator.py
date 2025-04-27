a05bcf872de51ed5e9a2bd72cd64ec8d8a3d9864
Pedro Serrano Faria <pedroserrano2@gmail.com>
2025-04-26 21:08:17 -0300

    feat: add ComparisonOperator enum for rule comparisons

---
+++ src/domain/rule/enums/comparison_operator.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+
+class ComparisonOperator(Enum):
+    EQUALS = "equals"
+    NOT_EQUALS = "not_equals"
+    CONTAINS = "contains"
+    NOT_CONTAINS = "not_contains"
+    GREATER_THAN = "greater_than"
+    LESS_THAN = "less_than"
+    REGEX = "regex"
